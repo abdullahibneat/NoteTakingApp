24c660cd4c440a346dd023f30c2c0ff3824254d8
abdullahibneat <iAbdullah@live.it>
2019-03-04 09:02:02 +0000

    Add JavaDoc

--- src/coursework/AllCourses.java
+++ src/coursework/AllCourses.java
@@ -1,31 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coursework;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 /**
+ * AllCourses class
+ * It holds all the Course objects within an array.
  *
  * @author Abdullah Ibne Atiq
  */
 public class AllCourses extends CommonCode {
     private final ArrayList<Course> allCourses = new ArrayList<>();
     
+    /**
+     * Constructor
+     */
     public AllCourses() {
         readAllCourses();
     }
 
+    /**
+     * Function to read all the courses from the "Course.txt" file.
+     */
     private void readAllCourses() {
         ArrayList<String> readCourses = new ArrayList<>();
         
         readCourses = readTextFile(appDir + "\\Courses.txt");
         System.out.println(readCourses.get(0));
 
-        if("File Not Found".equals(readCourses.get(0))){
+        if("File not found".equals(readCourses.get(0))){
         } else {
             for(String str: readCourses){                
                 Course c = new Course();
@@ -35,6 +38,11 @@ public class AllCourses extends CommonCode {
         }
     }
     
+    /**
+     * Function to add a new course to the ArrayList and store it permanently inside the "Courses.txt" file.
+     *
+     * @param course
+     */
     public void addCourse(String course) {
         Course c = new Course();
         c.setCourseName(course);
@@ -42,10 +50,18 @@ public class AllCourses extends CommonCode {
         writeAllCourses();
     }
     
+    /**
+     * Get all the courses as an ArrayList.
+     *
+     * @return
+     */
     public ArrayList<Course> getAllCourses() {
         return allCourses;
     }
 
+    /**
+     * Function to write courses to disk.
+     */
     private void writeAllCourses() {
         String path = appDir + "\\Courses.txt";
         ArrayList<String> writeCourse = new ArrayList<>();
--- src/coursework/Course.java
+++ src/coursework/Course.java
@@ -1,27 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coursework;
 
 /**
+ * Course class
+ * It is used to create a Course object with a course name.
  *
  * @author Abdullah Ibne Atiq
  */
 public class Course {
     private String courseName;
     
+    /**
+     * Constructor
+     */
     public Course() {
         
     }
     
+    /**
+     * Set the name of the course
+     *
+     * @param c Course name
+     */
     public void setCourseName(String c) {
         String crse = c;
         
         courseName = crse;
     }
     
+    /**
+     * Get the name of the course
+     *
+     * @return Course name
+     */
     public String getCourseName() {
         return courseName;
     }
